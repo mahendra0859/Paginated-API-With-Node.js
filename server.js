a336044c86032d15ff96f6b2a3827b76dd384442
mahendra <mahendra0859@gmail.com>
2019-11-06 18:13:58 +0530

    mongoose added

--- server.js
+++ server.js
@@ -1,53 +1,77 @@
-const express = require("express"),
-  app = express(),
-  users = [
-    { id: 1, name: "User 1" },
-    { id: 2, name: "User 2" },
-    { id: 3, name: "User 3" },
-    { id: 4, name: "User 4" },
-    { id: 5, name: "User 5" },
-    { id: 6, name: "User 6" },
-    { id: 7, name: "User 7" },
-    { id: 8, name: "User 8" },
-    { id: 9, name: "User 9" },
-    { id: 10, name: "User 10" },
-    { id: 11, name: "User 11" },
-    { id: 12, name: "User 12" },
-    { id: 13, name: "User 13" }
-  ],
-  posts = [
-    { id: 1, name: "Post 1" },
-    { id: 2, name: "Post 2" },
-    { id: 3, name: "Post 3" },
-    { id: 4, name: "Post 4" },
-    { id: 5, name: "Post 5" },
-    { id: 6, name: "Post 6" },
-    { id: 7, name: "Post 7" },
-    { id: 8, name: "Post 8" },
-    { id: 9, name: "Post 9" },
-    { id: 10, name: "Post 10" },
-    { id: 11, name: "Post 11" },
-    { id: 12, name: "Post 12" },
-    { id: 13, name: "Post 13" }
-  ];
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./user");
 
-app.get("/users", paginatedResults(users), (req, res) => {
-  res.json(res.paginatedresults);
+/** User Array */
+const users = [
+  { id: 1, name: "User 1" },
+  { id: 2, name: "User 2" },
+  { id: 3, name: "User 3" },
+  { id: 4, name: "User 4" },
+  { id: 5, name: "User 5" },
+  { id: 6, name: "User 6" },
+  { id: 7, name: "User 7" },
+  { id: 8, name: "User 8" },
+  { id: 9, name: "User 9" },
+  { id: 10, name: "User 10" },
+  { id: 11, name: "User 11" },
+  { id: 12, name: "User 12" },
+  { id: 13, name: "User 13" }
+];
+/** Posts Array */
+const posts = [
+  { id: 1, name: "Post 1" },
+  { id: 2, name: "Post 2" },
+  { id: 3, name: "Post 3" },
+  { id: 4, name: "Post 4" },
+  { id: 5, name: "Post 5" },
+  { id: 6, name: "Post 6" },
+  { id: 7, name: "Post 7" },
+  { id: 8, name: "Post 8" },
+  { id: 9, name: "Post 9" },
+  { id: 10, name: "Post 10" },
+  { id: 11, name: "Post 11" },
+  { id: 12, name: "Post 12" },
+  { id: 13, name: "Post 13" }
+];
+
+mongoose.connect(
+  "mongodb://localhost/pagination",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err =>
+    err ? console.log("Mongo Error", err) : console.info("Mongodb Connected")
+);
+
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await UserModel.countDocuments().exec()) > 0) return;
+  UserModel.create(users)
+    .then(() => console.log("Users Added"))
+    .catch(err => console.error("Mongo Error", err));
 });
-
-app.get("/posts", paginatedResults(posts), (req, res) => {
+// For Arrays
+// app.get("/users", paginatedResults(users), (req, res) => {
+//   res.json(res.paginatedresults);
+// });
+// app.get("/posts", paginatedResults(posts), (req, res) => {
+//     res.json(res.paginatedresults);
+//   });
+app.get("/users", paginatedResults(UserModel), (req, res) => {
   res.json(res.paginatedresults);
 });
 
 function paginatedResults(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
     const results = {};
-    if (endIndex < users.length) {
+    // For Array
+    // if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments())) {
       results.next = {
         page: page + 1,
         limit
@@ -59,10 +83,18 @@ function paginatedResults(model) {
         limit
       };
     }
-
-    results.data = model.slice(startIndex, endIndex);
-    res.paginatedresults = results;
-    next();
+    // For array
+    // results.data = model.slice(startIndex, endIndex);
+    try {
+      results.data = await model
+        .find()
+        .limit(limit)
+        .skip(startIndex);
+      res.paginatedresults = results;
+      next();
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   };
 }
 
